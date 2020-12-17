b1606b5857585a34b1ca2710b400d035d3d7e363
BorshWSC <ko5anev@gmail.com>
2020-12-17 14:01:22 +0700

    Deploy

--- src/main/java/ru/tpu/clouddelivery/config/WebSecurityConfig.java
+++ src/main/java/ru/tpu/clouddelivery/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.tpu.clouddelivery.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -12,5 +13,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll()
         .and().csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/**");
     }
 }
